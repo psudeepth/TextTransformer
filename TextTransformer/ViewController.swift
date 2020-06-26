eb0ed57d9a556d6a092c48819f987d9308aaf97b
Sudeepth Patinjarayil <psudeepth@gmail.com>
2020-06-26 22:49:00 +0530

    Initial Commit

---
+++ TextTransformer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TextTransformer
+//
+//  Created by Sudeepth Patinjarayil on 26/06/20.
+//  Copyright © 2020 Sudeepth Patinjarayil. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

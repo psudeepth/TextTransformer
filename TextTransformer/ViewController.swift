39a2044954e5a09ef0655d5a5c11399f14c9688a
Sudeepth Patinjarayil <psudeepth@gmail.com>
2020-06-27 22:22:29 +0530

    Initial upload. Application base completed. Application running in the menu bar.

--- TextTransformer/ViewController.swift
+++ TextTransformer/ViewController.swift
@@ -8,12 +8,15 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTextFieldDelegate {
+    @IBOutlet weak var TXT_input: NSTextField!
+    @IBOutlet weak var SEG_type: NSSegmentedControl!
+    @IBOutlet weak var TXT_output: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        TypeChanged(self)
     }
 
     override var representedObject: Any? {
@@ -22,6 +25,40 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func TypeChanged(_ sender: Any) {
+        switch SEG_type.selectedSegment{
+        case 0:
+            TXT_output.stringValue = rot13(TXT_input.stringValue)
+        case 1:
+            TXT_output.stringValue = similar(TXT_input.stringValue)
+        case 2:
+            TXT_output.stringValue = strike(TXT_input.stringValue)
+        default:
+            TXT_output.stringValue = zalgo(TXT_input.stringValue)
+        }
+    }
+    
+    @IBAction func CopyToClipboard(_ sender: Any) {
+    }
+    
+    func controlTextDidChange(_ obj: Notification) {
+        TypeChanged(self)
+    }
+    
+    func rot13(_ input: String) -> String{
+        return "ROT13: " + input
+    }
+    
+    func similar(_ input: String) -> String{
+        return "Similar: " + input
+    }
+    
+    func strike(_ input: String) -> String{
+        return "Strike: " + input
+    }
+    
+    func zalgo(_ input: String) -> String{
+        return "Zalgo: " + input
+    }
 }
 
